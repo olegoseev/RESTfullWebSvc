9d5af3ea5513d32ec6a9737d7762a67c3cde6ec7
Oleg Oseev <ooseev@hotmail.com>
2020-02-29 05:03:31 -0800

    Add project files.

---
+++ RESTfullWebSvc/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTfullWebSvc.Services;
+
+namespace RESTfullWebSvc.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository courseLibraryRepository)
+        {
+            _libraryRepository = courseLibraryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authors = _libraryRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _libraryRepository.GetAuthor(authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(author);
+        }
+    }
+}
